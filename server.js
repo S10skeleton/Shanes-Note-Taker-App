63ac606f6096f7ddec4007bb07b24bdb2d274707
Shane <s10skeleton@gmail.com>
2024-01-10 00:21:25 -0700

    changed port listener

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.listen(PORT, () =>
+app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`)
-);
+});
 
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
