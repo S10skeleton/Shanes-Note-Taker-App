f5407ee985685a495ccf0a3074e578a5886aadc2
Shane <s10skeleton@gmail.com>
2024-01-10 00:15:38 -0700

    port changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 
-const PORT = 3001;
+const PORT = process.env.Port || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
